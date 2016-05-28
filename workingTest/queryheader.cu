1ffeea224db7b6539b10cc9a93d748aa8cc92e3f
ubugu <claudio.ubugu@gmail.com>
2016-05-28 18:32:17 +0200

    [code] cosmetics

--- workingTest/queryheader.cu
+++ workingTest/queryheader.cu
@@ -36,6 +36,60 @@ int separateWords(std::string inputString, std::vector<std::string> &wordVector,
 	return 0;
 }
 
+//Make a select query on the store pointer saved on the
+template<typename element_t>
+std::vector<tripleContainer<element_t>*> ssselect(const std::vector<tripleContainer<element_t>*> d_selectQueries, const tripleContainer<element_t>* d_storePointer, const int storeSize, const element_t variableToken) {
+
+		int querySize =  d_selectQueries.size();
+		
+		standard_context_t context;		
+		auto compact = transform_compact(storeSize, context);
+		std::vector<tripleContainer<element_t>*> finalResultPointers;
+
+		for (int i = 0; i < querySize; i++) {
+			tripleContainer<element_t>* currentPointer = d_selectQueries[i]; 
+	             
+			int query_count = compact.upsweep([=] MGPU_DEVICE(int index) {
+				bool subjectEqual = false;
+				bool predicateEqual = false;
+				bool objectEqual = false;
+
+				if (currentPointer->subject != variableToken) {
+					subjectEqual = (currentPointer->subject == d_storePointer[index].subject);
+				} else {
+					subjectEqual = true;
+				}
+
+				if (currentPointer->predicate != variableToken) {
+				predicateEqual = (currentPointer->predicate == d_storePointer[index].predicate);
+				} else {
+				predicateEqual = true;
+				}
+	
+				if (currentPointer->object != variableToken) {
+					objectEqual = (currentPointer->object == d_storePointer[index].object);
+				} else {
+					objectEqual = true;
+				}
+
+				return (subjectEqual && predicateEqual && objectEqual);
+			});
+
+			tripleContainer<element_t>* d_result;
+			cudaMalloc(&d_result, query_count);
+
+			compact.downsweep([=] MGPU_DEVICE(int dest_index, int source_index) {
+				d_result[dest_index] = d_storePointer[source_index];
+			});
+			
+			finalResultPointers.push_back(d_result);
+		}
+		
+		return finalResultPointers;
+		
+}	
+
+
 int main(int argc, char** argv) {
 		using namespace std;
 		if (argc < 4 ) {
@@ -90,55 +144,3 @@ int main(int argc, char** argv) {
 }
 
 
-//Make a select query on the store pointer saved on the
-template<typename element_t>
-std::vector<tripleContainer<element_t>*> ssselect(const std::vector<tripleContainer<element_t>*> d_selectQueries, const tripleContainer<element_t>* d_storePointer, const int storeSize, const element_t variableToken) {
-
-		int querySize =  d_selectQueries.size();
-		
-		standard_context_t context;		
-		auto compact = transform_compact(storeSize, context);
-		std::vector<tripleContainer<element_t>*> finalResultPointers;
-
-		for (int i = 0; i < querySize; i++) {
-			tripleContainer<element_t>* currentPointer = d_selectQueries[i]; 
-	             
-			int query_count = compact.upsweep([=] MGPU_DEVICE(int index) {
-				bool subjectEqual = false;
-				bool predicateEqual = false;
-				bool objectEqual = false;
-
-				if (currentPointer->subject != variableToken) {
-					subjectEqual = (currentPointer->subject == d_storePointer[index].subject);
-				} else {
-					subjectEqual = true;
-				}
-
-				if (currentPointer->predicate != variableToken) {
-				predicateEqual = (currentPointer->predicate == d_storePointer[index].predicate);
-				} else {
-				predicateEqual = true;
-				}
-	
-				if (currentPointer->object != variableToken) {
-					objectEqual = (currentPointer->object == d_storePointer[index].object);
-				} else {
-					objectEqual = true;
-				}
-
-				return (subjectEqual && predicateEqual && objectEqual);
-			});
-
-			tripleContainer<element_t>* d_result;
-			cudaMalloc(&d_result, query_count);
-
-			compact.downsweep([=] MGPU_DEVICE(int dest_index, int source_index) {
-				d_result[dest_index] = d_storePointer[source_index];
-			});
-			
-			finalResultPointers.push_back(d_result);
-		}
-		
-		return finalResultPointers;
-		
-}	
