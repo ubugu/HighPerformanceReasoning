418bae41415e707514ea90205e81314855fae2a9
Claudio Sanna <sanna@sola1.dei.polimi.it>
2016-05-30 12:50:41 +0200

    Updated select function. Added size of the queries to the return type and the result pointer are now passed to a pointer variable

--- workingTest/queryheader.cu
+++ workingTest/queryheader.cu
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -21,6 +20,29 @@ struct tripleContainer {
         element_t object;
 };
 
+enum comparator_t {
+	LT,
+	LEQ,
+	EQ,
+	GT,
+	GEQ,
+	NN
+};
+
+template<typename comparator>
+std::function<bool()>  lambdaSelectCreator(bool negate)
+{
+	comparator compare[3];
+
+	if (negate == true) {
+		compare = !comparator();
+	} else {
+		compare = comparator();
+	}
+
+	return [=](){return compare(1,2);};
+}
+
 int separateWords(std::string inputString, std::vector<std::string> &wordVector,const char separator ) {	
 	const size_t zeroIndex = 0;
 	size_t splitIndex = inputString.find(separator);
@@ -36,16 +58,20 @@ int separateWords(std::string inputString, std::vector<std::string> &wordVector,
 	return 0;
 }
 
+
+
+
 //Make a select query on the store pointer saved on the
 template<typename element_t>
-std::vector<tripleContainer<element_t>*> ssselect(const std::vector<tripleContainer<element_t>*> d_selectQueries, const tripleContainer<element_t>* d_storePointer, const int storeSize, const element_t variableToken) {
+std::vector<int> rdfSelect(const std::vector<tripleContainer<element_t>*> d_selectQueries, const tripleContainer<element_t>* d_storePointer, const int storeSize, const element_t variableToken, std::vector<tripleContainer<element_t>*>** result) {
 
 		int querySize =  d_selectQueries.size();
 		
 		standard_context_t context;		
 		auto compact = transform_compact(storeSize, context);
 		std::vector<tripleContainer<element_t>*> finalResultPointers;
-
+		std::vector<int> resultsSize;
+	
 		for (int i = 0; i < querySize; i++) {
 			tripleContainer<element_t>* currentPointer = d_selectQueries[i]; 
 	             
@@ -83,10 +109,11 @@ std::vector<tripleContainer<element_t>*> ssselect(const std::vector<tripleContai
 			});
 			
 			finalResultPointers.push_back(d_result);
+			resultsSize.push_back(query_count);
 		}
-		
-		return finalResultPointers;
-		
+
+		*result = &finalResultPointers;
+		return resultsSize;
 }	
 
 
@@ -95,6 +122,9 @@ int main(int argc, char** argv) {
 		if (argc < 4 ) {
 			std::cout << "errore " << endl;
 		}
+
+
+		return 0;
 
                 const int FILE_LENGHT = 100000;
                 size_t rdfSize = FILE_LENGHT * sizeof(tripleContainer<int>);
@@ -135,11 +165,15 @@ int main(int argc, char** argv) {
 		cudaMemcpy(d_queryVector, h_queryVector, sizeof(tripleContainer<int>), cudaMemcpyHostToDevice);
 		
 		std::vector<tripleContainer<int>*> selectQuery;
-		std::vector<tripleContainer<int>*> selectResult;
+		std::vector<int> selectResult;
 		selectQuery.push_back(d_queryVector);
 		int token = -1;
-		selectResult = ssselect<int>(selectQuery, d_storeVector, FILE_LENGHT, token); 
-
+                std::vector<tripleContainer<int>*>** resultPointer = 0;
+		selectResult = rdfSelect<int>(selectQuery, d_storeVector, FILE_LENGHT, token, resultPointer); 
+		
+	//	cudaMemcpy(resultsss, selectResult[0] 
+		
+		
 		return 0;
 }
 
