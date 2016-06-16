b69dd2f19e9458e6e886ae0802bbf4b5b2a50828
Claudio Sanna <sanna@sola1.dei.polimi.it>
2016-06-16 20:19:28 +0200

    Added another java benchmark class for testing

--- Benchmark/App.java
+++ Benchmark/App.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,8 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
-import java.util.List;
-
+import java.util.*;
+import java.lang.Object;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.query.BindingSet;
@@ -37,13 +36,16 @@ public class App
         try {
 		repo.initialize();
 		RepositoryConnection con = repo.getConnection();
+
+		List timeVec = new LinkedList();
+		int N_CYCLES = 50;
 		
-		System.out.println("printo");
-		      
+
 		try {
+			con.add(file, baseURI, RDFFormat.NTRIPLES);
 			
-			con.add(file, baseURI, RDFFormat.NTRIPLES);     
-		     
+			
+
 		        long startTime = System.nanoTime();
 			String queryString = "SELECT ?p ?y WHERE { <http://example.org/int/0> ?p ?y . <http://example.org/int/0> ?p ?y } ";
 			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
@@ -51,66 +53,41 @@ public class App
 		    	TupleQueryResult result = tupleQuery.evaluate();
 		    	resultList = QueryResults.asList(result);
 		    	long endTime = System.nanoTime();
-			long duration = (endTime - startTime);
-		    	System.out.println(duration / 1000000);
-		      
-		       
+			Long duration = new Long((endTime - startTime) / 100000);
+//		    	System.out.println(i);
+			System.out.println(duration.longValue() );
+			timeVec.add(duration);
+			
+			
+			
+			long mean = 0;
+			long variance = 0;
+			for (Object value : timeVec) {
+				
+				Long value2 = (Long) value;
+				System.out.println(value2.longValue());
+				mean += value2.longValue();
+				variance += value2.longValue() * value2.longValue();
+			}
+			mean = mean / timeVec.size();
+			variance = variance / timeVec.size();
+			variance = variance - mean * mean;
+			System.out.println("Variance is " + variance);
+			System.out.println("Mean is " + mean);
+
 	   	}   catch (Exception e) {
-	   		
+
 	   		System.out.println("inner error");
-	   		System.out.println(e);      
+	   		System.out.println(e);
 	      	}
-	     
-	   	
-	   	
+		
+
 	} catch (Exception e) {
-	
+
 		System.out.println("outer error");
 		e.printStackTrace();
-	
 	}
-        
-        System.out.println("Funza");
-    }
-    
-    
-    public static void copy() throws IOException
-    {
-        File file =  new File("C:/Users/Claudio/Downloads/eclipse-rdf4j-2.0M2-sdk/rdf.txt");
-        
-        
-			String baseURI = "http://example.org/int/";
-			String sCurrentLine;
-			BufferedReader br;
-			br = new BufferedReader(new FileReader("C:/Users/Claudio/Downloads/eclipse-rdf4j-2.0M2-sdk/rdf.txt"));
-			PrintWriter writer = new PrintWriter("C:/Users/Claudio/Downloads/eclipse-rdf4j-2.0M2-sdk/rdf2.txt", "UTF-8");
-			int index = 1;
-			while ((sCurrentLine = br.readLine()) != null) {
-				String[] splitted = sCurrentLine.split(" ");
-				splitted[0] = "<" + baseURI + splitted[0] + ">";
-				splitted[1] = "<" + baseURI + splitted[1] + ">";
-				splitted[2] = "<" + baseURI + splitted[2] + ">";
-				if (index % 77 == 0) {
-					splitted[0] = "<" + baseURI + "0" + ">";
-				}
-				writer.println(splitted[0] + " " +  splitted[1] + " " +  splitted[2] + " " +  splitted[3]);
-				index++;
-			}
-			
-			try {
-			      //con.add(file, baseURI, RDFFormat.NTRIPLES);
-			      URL url = new URL("http://example.org/example/remote.rdf");
-			     
-			 
-		   }
-	       
-	       catch (Exception e) {
-	        	   // handle exception
-	       }
-	           
-	
 
-		
-        
+        System.out.println("Funza");
     }
 }
